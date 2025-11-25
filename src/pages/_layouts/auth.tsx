5e00883e2402cecb0822d4b132ba9c7a1712929f
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-11-24 22:07:42 -0300

    feat(pages): create sign-in page

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,10 +1,20 @@
+import { Pizza } from "lucide-react";
 import { Outlet } from "react-router-dom";
 
 export function AuthLayout() {
   return (
-    <div>
-      <h1>Authentication</h1>
-      <div>
+    <div className="min-h-screen grid grid-cols-2">
+      <div className="h-full border-r border-foreground/5 bg-muted p-10 
+        text-muted-foreground flex flex-col justify-between">
+          <div className="flex items-center gap-3 text-lg text-foreground">
+            <Pizza className="h-5 w-5" />
+            <span className="font-semibold">Pizza Shop</span>
+          </div>
+          <footer className="text-sm">
+            Painel do Parceiro &copy; Pizza Shop - {new Date().getFullYear()}
+          </footer>
+        </div>
+      <div className="flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 
 export function SignIn() {
@@ -6,7 +9,27 @@ export function SignIn() {
       <Helmet 
         title="Sign-In"
       />
-      <div>Sign In Page</div>
+      <div className="p-8">
+        <div className="w-[350px] flex flex-col justify-center gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro!
+            </p>
+          </div>
+          <div className="">
+            <form className="space-y-4">
+              <div className="space-y-2">
+                <Label htmlFor="email">Seu e-mail:</Label>
+                <Input id="email" type="email" />
+              </div>
+              <Button className="w-full" type="submit">Acessar painel</Button>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
