be57ba138651b0d700e2427f092389a82208ff33
Patrick Cuppi <cuppi.patrick@gmail.com>
2025-12-14 15:01:43 -0300

    feat: add theme toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
 import "./index.css";
 import { router } from "./routes";
-import { Helmet, HelmetProvider } from "react-helmet-async";
-import { Toaster } from "sonner";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet
-        titleTemplate="%s | Pizza Shop"
-      />
-      <Toaster position="top-right" richColors closeButton />
-      <RouterProvider router={router} />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet
+          titleTemplate="%s | Pizza Shop"
+        />
+        <Toaster position="top-right" richColors closeButton />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
