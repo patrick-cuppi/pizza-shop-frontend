ad60b4f0cf34c0a850436e6483c7ac4abb20ecf6
Patrick Cuppi <cuppi.patrick@gmail.com>
2026-01-07 19:58:36 -0300

    feat: create orders page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
-import { Dashboard } from "./pages/app/dashboard";
-import { SignIn } from "./pages/auth/sign-in";
 import { AppLayout } from "./pages/_layouts/app";
 import { AuthLayout } from "./pages/_layouts/auth";
+import { Dashboard } from "./pages/app/dashboard";
+import { OrdersPage } from "./pages/app/orders/orders";
+import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 
 export const router = createBrowserRouter([
@@ -11,6 +12,7 @@ export const router = createBrowserRouter([
     element: <AppLayout />,
     children: [
       { path: '/', element: <Dashboard /> },
+      { path: '/orders', element: <OrdersPage /> },
     ]
   },
   {
