d7e613148de822c91a89462fca4d6a151a4a5456
Patrick Cuppi <cuppi.patrick@gmail.com>
2026-01-19 18:44:59 -0300

    feat: register a new restaurant

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -5,7 +5,7 @@ import { Label } from "@/components/ui/label";
 import { useMutation } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useSearchParams } from "react-router-dom";
 import { toast } from "sonner";
 import z from "zod";
 
@@ -16,11 +16,17 @@ const signInFormSchema = z.object({
 type SignInFormSchema = z.infer<typeof signInFormSchema>;
 
 export function SignIn() {
+  const [searchParams] = useSearchParams();
+
   const {
     register,
     handleSubmit,
     formState: { isSubmitting },
-  } = useForm<SignInFormSchema>();
+  } = useForm<SignInFormSchema>({
+    defaultValues: {
+      email: searchParams.get("email") ?? "",
+    },
+  });
 
   const { mutateAsync: authenticate } = useMutation({
     mutationFn: signIn,
@@ -29,8 +35,6 @@ export function SignIn() {
   async function handleSignIn(data: SignInFormSchema) {
     try {
       await authenticate({ email: data.email });
-
-      console.log(data);
 
       toast.success(
         "E-mail enviado com sucesso! Verifique sua caixa de entrada.",
