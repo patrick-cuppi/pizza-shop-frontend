764bb3052cae67d4d2043a4c51d3a15645c0c414
Patrick Cuppi <cuppi.patrick@gmail.com>
2026-01-22 20:25:25 -0300

    feat: add metric cards to dashboard

--- src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
+++ src/pages/app/dashboard/month-canceled-orders-amount-card.tsx
@@ -1,7 +1,14 @@
+import { getMonthCanceledOrdersAmount } from "@/api/get-month-canceled-orders-amount";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
 import { TicketX } from "lucide-react";
 
 export function MonthCanceledOrdersAmountCard() {
+  const { data: monthCanceledOrdersAmount } = useQuery({
+    queryKey: ["metrics", "month-canceled-orders-amount"],
+    queryFn: getMonthCanceledOrdersAmount,
+  });
+
   return (
     <Card>
       <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
@@ -11,10 +18,32 @@ export function MonthCanceledOrdersAmountCard() {
         <TicketX className="h-4 w-4 text-muted-foreground" />
       </CardHeader>
       <CardContent className="space-y-1">
-        <span className="text-2xl font-bold tracking-tight">5</span>
+        <span className="text-2xl font-bold tracking-tight">
+          {monthCanceledOrdersAmount?.amount.toLocaleString("pt-BR")}
+        </span>
         <p className="text-xs text-muted-foreground">
-          <span className="text-rose-500 dark:text-rose-400">+20%</span> em
-          relação ao último mês
+          {monthCanceledOrdersAmount &&
+          monthCanceledOrdersAmount.diffFromLastMonth >= 0 ? (
+            <>
+              <span className="text-rose-500 dark:text-rose-400">
+                +
+                {monthCanceledOrdersAmount?.diffFromLastMonth.toLocaleString(
+                  "pt-BR",
+                )}
+                %
+              </span>
+            </>
+          ) : (
+            <>
+              <span className="text-emerald-500 dark:text-emerald-400">
+                {monthCanceledOrdersAmount?.diffFromLastMonth.toLocaleString(
+                  "pt-BR",
+                )}
+                %
+              </span>
+            </>
+          )}{" "}
+          em relação ao último mês
         </p>
       </CardContent>
     </Card>
