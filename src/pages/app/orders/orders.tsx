10450978c7e2a47ed4b20731eba07c37cfe339f6
Patrick Cuppi <cuppi.patrick@gmail.com>
2026-01-23 17:22:26 -0300

    feat: add a skeleton in orders and orders details

--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -13,6 +13,7 @@ import { useSearchParams } from "react-router-dom";
 import z from "zod";
 import { OrderTableFilter } from "./order-table-filter";
 import { OrderTableRow } from "./order-table-row";
+import { OrderTableSkeleton } from "./order-table-skeleton";
 
 export function OrdersPage() {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -26,7 +27,7 @@ export function OrdersPage() {
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -68,6 +69,7 @@ export function OrdersPage() {
                 </TableRow>
               </TableHeader>
               <TableBody>
+                {isLoadingOrders && <OrderTableSkeleton />}
                 {result &&
                   result.orders.map((order) => {
                     return <OrderTableRow key={order.orderId} order={order} />;
