3b28c8552817168730e6490c93207a5271e655fd
Patrick Cuppi <cuppi.patrick@gmail.com>
2026-01-23 17:33:50 -0300

    feat: update the error pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,12 +6,13 @@ import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { OrdersPage } from "./pages/app/orders/orders";
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
+import { Error } from "./pages/error";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: "/", element: <Dashboard /> },
       { path: "/orders", element: <OrdersPage /> },
@@ -24,5 +25,9 @@ export const router = createBrowserRouter([
       { path: "/sign-in", element: <SignIn /> },
       { path: "/sign-up", element: <SignUp /> },
     ],
+  },
+  {
+    path: "*",
+    element: <NotFound />,
   },
 ]);
