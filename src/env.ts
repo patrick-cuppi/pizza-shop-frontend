4c931b3ee1064d61f0819a36bbcb60e449d09c28
Patrick Cuppi <cuppi.patrick@gmail.com>
2026-01-27 13:44:55 -0300

    test: create a sign-in mock

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import z from "zod";
 
 const envSchema = z.object({
-  VITE_API_URL: z.url(),
+  VITE_API_URL: z.string(),
   VITE_ENABLE_API_DELAY: z.string().transform((value) => value === "true"),
   MODE: z.enum(["development", "production", "test"]),
 });
