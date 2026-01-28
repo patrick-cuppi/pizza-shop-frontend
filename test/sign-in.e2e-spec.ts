f7a35ecb797ac974e4b6e793e0fcdc7ae156bdd5
Patrick Cuppi <cuppi.patrick@gmail.com>
2026-01-28 16:14:05 -0300

    test: create sign-in e2e

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { expect, test } from "@playwright/test";
+
+test("sign in successfully", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page
+    .getByRole("textbox", { name: "Seu e-mail:" })
+    .fill("johndoe@example.com");
+
+  await page.getByRole("button", { name: "Acessar painel" }).click();
+
+  const toast = page.getByText(
+    "E-mail enviado com sucesso! Verifique sua caixa de entrada.",
+  );
+
+  expect(toast).toBeVisible();
+
+  await page.waitForTimeout(2000);
+});
+
+test("sign in with wrong credentials", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page
+    .getByRole("textbox", { name: "Seu e-mail:" })
+    .fill("wrong-email@example.com");
+
+  await page.getByRole("button", { name: "Acessar painel" }).click();
+
+  const toast = page.getByText(
+    "Ocorreu um erro ao enviar o e-mail. Tente novamente.",
+  );
+
+  expect(toast).toBeVisible();
+
+  await page.waitForTimeout(2000);
+});
+
+test("navigate to register a new restaurant page", async ({ page }) => {
+  await page.goto("/sign-in", { waitUntil: "networkidle" });
+
+  await page.getByRole("link", { name: "Novo estabelecimento" }).click();
+
+  expect(page.url()).toContain("/sign-up");
+});
